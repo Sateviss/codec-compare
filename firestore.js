1e693afb3fc1cb884cef3810020b1ab4ef989552
Eugene Pisarchik <sateviss@gmail.com>
2024-08-24 22:41:46 +0200

    Added firestore

---
+++ firestore.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
+import * as FS from "https://www.gstatic.com/firebasejs/10.13/firebase-firestore.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyBQB1r3-qZl55cX9DobDni58uy9ncDBcgU",
+    authDomain: "test-a4e83.firebaseapp.com",
+    projectId: "test-a4e83",
+    storageBucket: "test-a4e83.appspot.com",
+    messagingSenderId: "313802864526",
+    appId: "1:313802864526:web:ab9e8c42b8b8ca0c5556df",
+    measurementId: "G-LNDVJXJ7J8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(window.app);
+const firestore = FS.getFirestore(window.app);
+
+window.getStatsForSong = async function (songId) {
+    let out = {};
+    let formats = ["aac", "mp3", "flac"];
+    const collection = FS.collection(firestore, "responses");
+    for (const format of formats) {
+        const query = FS.query(collection, FS.where(songId, "==", format));
+        const count = await FS.getCountFromServer(query);
+        out[format] = count.data().count;
+    }
+    return out;
+}
+
+window.sendResponse = async function (response) {
+    await FS.setDoc(
+        FS.doc(firestore, "responses", crypto.randomUUID()),
+        response
+    )
+}
--- index.js
+++ index.js
@@ -17,16 +17,17 @@ const FORMAT_DICT = {
 }
 
 let songOrder = [];
-
+let app;
+let firestore;
 
 function shuffle(array) {
-  let currentIndex = array.length;
-  while (currentIndex != 0) {
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
 }
 
 async function selectSong(songName) {
@@ -40,12 +41,12 @@ async function selectSong(songName) {
     document.querySelector("#song_img").alt = `Cover for ${songName}`;
     document.querySelector("#song_id").innerText = songName;
     document.querySelector("#song_data").innerText = songInfo;
-    
+
     let order = ["flac", "mp3", "aac"]
     shuffle(order);
     for (let i = 1; i <= 3; i++) {
-        document.querySelector(`#track${i} audio`).src = `./songs/${songName}/song.${order[i-1]}`;
-        document.querySelector(`#track${i} button`).format = FORMAT_DICT[order[i-1]];
+        document.querySelector(`#track${i} audio`).src = `./songs/${songName}/song.${order[i - 1]}`;
+        document.querySelector(`#track${i} button`).format = order[i - 1];
     }
 }
 
@@ -62,20 +63,46 @@ async function handleSelection(event) {
 
 async function showResults() {
     console.log(SONGS);
+    if (window.sendResponse) {
+        await window.sendResponse(SONGS);
+    }
     document.querySelector("#song_info").hidden = true;
     document.querySelector(".track_container").hidden = true;
     const table = document.querySelector("table");
     table.hidden = false;
-    Object.keys(SONGS).forEach(songId => {
+    for (const songId of Object.keys(SONGS)) {
         const row = document.createElement("tr");
         const songIdCell = document.createElement("td");
         const choiceCell = document.createElement("td");
+        const flacCell = document.createElement("td");
+        const aacCell = document.createElement("td");
+        const mp3Cell = document.createElement("td");
+        const nCell = document.createElement("td");
+
         songIdCell.innerText = songId;
-        choiceCell.innerText = SONGS[songId];
+        choiceCell.innerText = FORMAT_DICT[SONGS[songId]];
+        flacCell.innerText = "?";
+        aacCell.innerText = "?";
+        mp3Cell.innerText = "?";
+        nCell.innerText = "?";
         row.appendChild(songIdCell);
         row.appendChild(choiceCell);
+        row.appendChild(flacCell);
+        row.appendChild(aacCell);
+        row.appendChild(mp3Cell);
+        row.appendChild(nCell);
+
+        if (window.getStatsForSong) {
+            const stats = await window.getStatsForSong(songId);
+            const total = Object.keys(stats).reduce((acc, cur) => acc + stats[cur], 0);
+            nCell.innerText = total.toString();
+            flacCell.innerText = (100.0*(stats["flac"]||0)/(1.0*total)).toFixed(2);
+            aacCell.innerText = (100.0*(stats["aac"]||0)/(1.0*total)).toFixed(2);
+            mp3Cell.innerText = (100.0*(stats["mp3"]||0)/(1.0*total)).toFixed(2);
+        }
+
         table.appendChild(row);
-    })
+    };
 }
 
 window.onload = async () => {
